e56fe513bed0f02ca1240ad3d8a829348634aeeb
Dan Kempster <dev@dankempster.co.uk>
2014-12-30 19:34:40 +0000

    Add copyright notice and license info

--- Report/Factory.php
+++ Report/Factory.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of the Axstrad library.
+ *
+ * (c) Dan Kempster <dev@dankempster.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2014-2015 Dan Kempster <dev@dankempster.co.uk>
+ */
+
 namespace Axstrad\Component\WorkForce\Report;
 
 use PhpOption\Option;
